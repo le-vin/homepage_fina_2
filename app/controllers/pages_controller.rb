cffcd4241cf15992f05cb20038de09fbd15c0622
Levin Bauschke <b.bauschke@zeppelin-university.net>
2022-06-26 23:24:14 +0200

    frontend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,3 @@
 class PagesController < ApplicationController
-    def home
-    end
+    @pic_link = "https://res.cloudinary.com/dwyjybcee/image/upload/v1655250637/fina_me/FullSizeRender_bvldiv.jpg"
 end
